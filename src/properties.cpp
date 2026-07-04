28e2b5f907c39700c2e6869602d211ebdd312d8a
mrenault07 <renault.manon07@gmail.com>
2026-07-04 16:43:05 +0200

    Properties First init

---
+++ src/properties.cpp
@@ -0,0 +1,29 @@
+/*
+Exercices from : https://pynative.com/cpp-oop-exercises/
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+// 1
+class Rectangle {
+private:
+    int length = 10;
+    int width = 5;
+public :
+    int calculate_area(){
+        return length * width;
+    }
+    int calculate_perimeter(){
+        return 2 * length + 2 * width;
+    }
+};
+
+int main(){
+    Rectangle rect;
+    cout << "Perimeter : " << rect.calculate_perimeter();
+    cout << "\nArea : " << rect.calculate_area();
+    return 0;
+}
